6572812574af3fa69233b191b0bca3d40e11a888
Christopher-isu <christopherzakrev@isu.edu>
2025-02-09 20:23:36 -0700

    Add project files.

---
+++ FortuneCookie/FortuneCookie.vb
@@ -0,0 +1,21 @@
+﻿'ChristopherZ
+'Spring 2025
+'RCET2265
+'Fortune Cookie
+'
+
+Option Explicit On 'forces all variables to be declared
+Option Strict On 'forces all data types to match
+
+Module FortuneCookie
+
+    Sub Main()
+    Function RandomNumberBetween(min As Integer, max As Integer) As Integer
+        Randomize()
+        Static rnd As New Random()
+        Return rnd.Next(min, max + 1)
+    End Function
+
+    End Sub
+
+End Module
---
+++ FortuneCookie/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FortuneCookie")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FortuneCookie")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b1f5f732-2c22-46ee-a943-5b33b37bb1b6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
